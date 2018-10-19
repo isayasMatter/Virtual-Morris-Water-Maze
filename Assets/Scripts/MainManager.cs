196a7b7f728b9d3aef500a605420d59d0b1e58bb
Isayas Adhanom <isayasadhanom@gmail.com>
2018-10-19 11:50:57 -0700

    Trial timer and JSON parser added

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -132,6 +132,9 @@ public class MainManager : MonoBehaviour {
 		InsertPlayer();		
 	}
 
+	void InsertPlayer(){
+		
+	}
 	
 
 }
---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,9 @@
+using System;
+
+[Serializable]
+public class Position{
+	public int ID;
+	public float posX;
+    public float posY;
+    public float posZ;
+}
